5174c989a6f912d2ed23c9937b5fc29bf62d438e
bluepome98 <wpdn1998@gmail.com>
2025-03-31 15:02:17 +0900

    🎉 add/ Supabase 데이터 테이블 타입 생성

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+export default function Header() {
+  return (
+    <div>
+      <Link href="community">commnunity</Link>
+      <Link href="note">note</Link>
+      <Link href="letter">letter</Link>
+      <Link href="login">login</Link>
+      <Link href="signup">signup</Link>
+      <Link href="ranking">ranking</Link>
+      <Link href="profile">profile</Link>
+    </div>
+  );
+}

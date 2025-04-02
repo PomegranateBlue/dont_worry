2c0fb97d9db2febada4f3442f9de525a866962b5
answq <wnffldksla@gmail.com>
2025-04-02 11:04:21 +0900

    🎉add/ supabase에 편지 데이터 저장하는 로직 추가

--- src/app/letter/page.tsx
+++ src/app/letter/page.tsx
@@ -1,5 +1,25 @@
-const page = () => {
-  return;
+import { supabase } from '../utils/supabase/supabase';
+
+type LetterProps = {
+  userId: string;
+  content: string;
+  sendAt: string;
+  imageUrl: string;
 };
 
-export default page;
+const LetterPage = async ({
+  userId,
+  content,
+  sendAt,
+  imageUrl
+}: LetterProps) => {
+  const { data, error } = await supabase
+    .from('letters')
+    .insert([{ user_id: userId, content, send_at: sendAt, img_url: imageUrl }]);
+  if (error) {
+    console.error('편지 저장 실패', error);
+  }
+  return data;
+};
+
+export default LetterPage;

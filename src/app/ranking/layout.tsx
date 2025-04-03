1acee383396e8159a9d04c9ff90962b86857cbd4
mzsoba <owt4832@gmail.com>
2025-04-03 17:10:53 +0900

    ✨ feat/ 통계 페이지 상단부 메뉴 선택 추가

--- src/app/ranking/layout.tsx
+++ src/app/ranking/layout.tsx
@@ -1,7 +1,16 @@
+import FilterMenu from '@/components/ranking/FilterMenu';
+import RankingHeader from '@/components/ranking/RankingHeader';
+
 const RankingLayout = ({
   children
 }: Readonly<{ children: React.ReactNode }>) => {
-  return <>{children}</>;
+  return (
+    <>
+      <RankingHeader />
+      <FilterMenu />
+      {children}
+    </>
+  );
 };
 
 export default RankingLayout;

5c2ce25ef2accca946e7e4874e1f0c427a7a958d
mzsoba <owt4832@gmail.com>
2025-04-03 17:12:41 +0900

    🚧 test/ 클라이언트 컴포넌트, 서버 컴포넌트로 슈퍼베이스 로직 테스트 중입니다

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,51 +1,42 @@
 'use client';
-
-import React from 'react';
-import { Doughnut } from 'react-chartjs-2'; //원하는 차트 형태를 임포트
-import {
-  Chart as ChartJS, //chart.js의 핵심 객체(필수)
-  ChartOptions, //옵션 타입
-  ChartData, //데이터 타입
-  ArcElement, //도넛 차트에서 각 조각ㅇ르 그리는 요소
-  Tooltip,//마우스를 올렸을때 나오는 메타데이터?임
-  Legend//각 조각들에 대한 범례라고함(궁금할경우 담당자에게 문의)
-} from 'chart.js'; //chart.js에서 차트를 만들때 필요한 모둘을 가져옴
-
-// 도넛 그래프에 필요한 요소 등록(필요 요소가 부족하면 그래프가 그려지지 않을 수 있음)
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-const options: ChartOptions<'doughnut'> = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'bottom'
-    }
-  }
-};
-
-const labels = ['React', 'Angular', 'Vue']; //실제 구현시 여기에 자주 언급된 카테괼 top10이 들어가게됨
-
-const data: ChartData<'doughnut'> = {
-  //차트에 들어가는 각각의 비율을 정의하는 부분
-  labels,
-  datasets: [
-    {
-      label: 'Usage Percentage',
-      data: [60, 25, 15], // 도넛 차트는 비율(%) 형태의 데이터가 적합
-      backgroundColor: ['#0CD3FF', '#a6120d', '#FFCA29'],
-      borderColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF'],
-      borderWidth: 2
-    }
-  ]
-};
+import Chart from '@/components/ranking/Chart';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../utils/supabase/supabase';
+import type { Tables } from '../../../database.types';
 
 const RankingPage = () => {
+  const [test, setTest] = useState<Tables<'users_note'>[]>([]);
+  useEffect(() => {
+    const getData = async () => {
+      const { data, error } = await supabase.from('users_note').select('*');
+      if (error) {
+        console.log(error);
+      }
+      console.log('fetcheddata=>', data);
+      setTest(data ?? []);
+    };
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    console.log('Updated test:', test);
+  }, [test]);
+
+  // const { data, error } = await supabase.from('users_note').select('*');   async잊지 말고 붙이세요
+
+  // if (error) {
+  //   console.error('Error fetching data:', error);
+  //   return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
+  // }
+
+  // console.log(data); // data가 실제 배열인지 확인
+  // const res = JSON.stringify(data); // data를 문자열로 변환
+  // console.log('res=>', res);
+
   return (
     <div>
-      <h1>Doughnut Chart</h1>
-      <div style={{ width: 400, height: 400 }}>
-        <Doughnut options={options} data={data} />
-      </div>
+      <Chart />
     </div>
   );
 };

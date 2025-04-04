4691dc292517f65f410a1954ddf15ef39a8297f2
bluepome98 <wpdn1998@gmail.com>
2025-04-04 11:35:39 +0900

    🎉 add / 소개 페이지 이미지 로드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 const HomePage = () => {
   return (
-    <section></section>
-  )
+    <main>
+      <section className="w-full h-[482px]">Hero section</section>
+      <section className="w-full h-[110px]"></section>
+      <section className="w-full h-[110px]"></section>
+      <section className="w-full h-[110px]"></section>
+    </main>
+  );
 };
 
 export default HomePage;

9b790bd509be6ab94aaaef93aac437169e3267b0
hyerin-kang <hkang0420@gmail.com>
2025-04-04 20:49:53 +0900

    ✨ feat / 로그인 여부에 따른 헤더 조건부 렌더링

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,32 +4,24 @@ import LogOutButton from './loginComponents/LogOutButton';
 import { useEffect, useState } from 'react';
 import { Session } from '@supabase/supabase-js';
 import { supabase } from '@/app/utils/supabase/supabase';
-// import browserClient from '@/app/utils/supabase/client';
 
 export default function Header() {
   const [session, setSession] = useState<Session | null>(null);
+  const isLogin = !!session;
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
     const {
       data: { subscription }
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      // if (session) {
-      //   setSession(session);
-      // }
-      setSession(session);
-      console.log('session', session);
-
-      console.log('_event', _event);
+      if (_event == 'SIGNED_IN') {
+        setSession(session);
+        console.log('useEffect session', session);
+      }
     });
 
     return () => {
       subscription.unsubscribe();
     };
   }, []);
-  console.log('session: ', session);
 
   return (
     <header className="flex flex-row flex-wrap bg-slate-200 p-4 justify-between">
@@ -39,9 +31,14 @@ export default function Header() {
       <Link href="/ranking">통계 페이지</Link>
       <Link href="/letter">편지쓰기</Link>
       <Link href="/mypage">마이페이지</Link>
-      <Link href="/auth/login">로그인</Link>
-      <Link href="/auth/signup">회원가입</Link>
-      <LogOutButton />
+      {isLogin ? (
+        <LogOutButton />
+      ) : (
+        <>
+          <Link href="/auth/login">로그인</Link>
+          <Link href="/auth/signup">회원가입</Link>
+        </>
+      )}
     </header>
   );
 }

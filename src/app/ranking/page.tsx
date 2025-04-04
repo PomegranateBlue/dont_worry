7a99dd1e99764d4ad557dded2bdaf15563125aaf
mzsoba <owt4832@gmail.com>
2025-04-04 19:48:55 +0900

    ✨

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,38 +1,33 @@
-'use client';
 import Chart from '@/components/ranking/Chart';
-import React, { useEffect, useState } from 'react';
-import { supabase } from '../utils/supabase/supabase';
-import { UsersNote } from '@/types/ranking/types';
+import React from 'react';
+import { createClient } from '../utils/supabase/server';
+import {
+  makeTopTen,
+  counttMentionedKeyword
+} from '../utils/ranking/RankingFilter';
 
-const RankingPage = () => {
-  const [test, setTest] = useState<UsersNote[]>([]);
-  useEffect(() => {
-    const getData = async () => {
-      const { data, error } = await supabase.from('users_note').select('*');
-      if (error) {
-        console.log(error);
-      }
-      console.log('fetcheddata=>', data);
-      setTest(data ?? []);
-    };
+const RankingPage = async () => {
+  const serverClient = createClient(); //서버 컴포넌트여서 사용해야함
 
-    getData();
-  }, []);
+  const { data: userCategory, error } = await serverClient
+    .from('users_note')
+    .select('*'); //나중에 아이디랑 날짜로 필터링을 한번더 거쳐야함
 
-  useEffect(() => {
-    console.log('Updated test:', test);
-  }, [test]);
+  if (error) {
+    console.error('Error fetching data:', error);
+    return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
+  }
 
-  // const { data, error } = await supabase.from('users_note').select('*');   async잊지 말고 붙이세요
+  if (userCategory && userCategory.length > 0) {
+    const keywordAnalysis = countMentionedKeyword(userCategory);
+    console.log('키워드별 언급 횟수:', keywordAnalysis);
 
-  // if (error) {
-  //   console.error('Error fetching data:', error);
-  //   return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
-  // }
-
-  // console.log(data); // data가 실제 배열인지 확인
-  // const res = JSON.stringify(data); // data를 문자열로 변환
-  // console.log('res=>', res);
+    const topKeywords = makeTopTen(userCategory);
+    console.log('상위 10개 주제:', topKeywords.topTopics);
+    console.log('상위 10개 감정:', topKeywords.topEmotions);
+  } else {
+    console.log('데이터가 없거나 비어있습니다.');
+  }
 
   return (
     <div>

e73c1cbffb0c5a722554acc00a8abfcca802678e
mzsoba <owt4832@gmail.com>
2025-04-04 19:50:18 +0900

    ✨ feat/ 상위 10개 항목, 각 항목별 언급 횟수 필터링 유틸 함수 추가

---
+++ src/app/utils/ranking/RankingFilter.ts
@@ -0,0 +1,55 @@
+import {
+  CategoryCounts,
+  KeywordAnalysis,
+  RankingItem,
+  TopTenRanking,
+  UserNote
+} from '@/types/ranking/types';
+
+export const countMentionedKeyword = (
+  //각 키워드별 언급된 횟수를 카운트하는 함수
+  userCategory: UserNote[]
+): KeywordAnalysis => {
+  const topicCounts: CategoryCounts = {};
+  const emotionCounts: CategoryCounts = {};
+
+  userCategory.forEach((note) => {
+    if (note.topic_category) {
+      const topics = note.topic_category.split(', ');
+      topics.forEach((topic) => {
+        topicCounts[topic] = (topicCounts[topic] || 0) + 1;
+      });
+    }
+
+    if (note.emotion_category) {
+      const emotions = note.emotion_category.split(', ');
+      emotions.forEach((emotion) => {
+        emotionCounts[emotion] = (emotionCounts[emotion] || 0) + 1;
+      });
+    }
+  });
+
+  return {
+    topics: topicCounts,
+    emotions: emotionCounts
+  };
+};
+
+export const makeTopTen = (userCategory: UserNote[]): TopTenRanking => { //키워드별 상위 10개의 카테고리를 리턴하는 함수
+  const { topics, emotions } = countMentionedKeyword(userCategory);
+
+  const topTopics: RankingItem[] = Object.entries(topics)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10)
+    .map(([name, count]) => ({ name, count }));
+
+  const topEmotions: RankingItem[] = Object.entries(emotions)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10)
+    .map(([name, count]) => ({ name, count }));
+
+  return {
+    topTopics,
+    topEmotions
+  };
+};

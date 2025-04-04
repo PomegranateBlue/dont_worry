9be610e5e93234167e83c1c84f3231ca7f18dade
bluepome98 <wpdn1998@gmail.com>
2025-04-04 10:21:53 +0900

    🚚 move / 헤더 제작 취소 후 파일 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 const HomePage = () => {
   return <p>This is Home Page</p>;
-=======
-
-import ChatBox from '@/components/main/modules/chatBox';
-
-const HomePage = () => {
-  return <ChatBox />;
-
->>>>>>> 2249b3bec95b833142f0fc2a55a542f3833439f4
 };
 
-export default page;
+export default HomePage;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+
 export default function Header() {
   return (
     <div className="w-full bg-white h-[48px] text-black   ">

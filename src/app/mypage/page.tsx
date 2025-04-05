ed2a6559aa10cb2f0b10c0005c6bf563c8cbcb9d
ick-web <yih000098@gmail.com>
2025-04-05 13:27:00 +0900

    ✨ update: userid 불러오는 로직 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,17 +1,40 @@
-import { useUserInfo, useUserLetters } from '@/hooks/useMyPageQueries';
+'use client';
+
+// import { useAuth } from '@/hooks/useAuth';
+import {
+  useUserData,
+  useUserInfo,
+  useUserLetters
+} from '@/hooks/useMyPageQueries';
 
 const MyPage = () => {
+  // const { user, loading: authLoading } = useAuth();
+  const {
+    data: userid,
+    isPending: isUserPending,
+    isError: isUserError
+  } = useUserData();
   const { data: letters, isLoading: lettersLoading } = useUserLetters();
-  const { data, isLoading } = useUserInfo();
+  const { data: userData, isLoading: userDataLoading } = useUserInfo();
 
-  if (lettersLoading) {
-    return <div>Loading...</div>;
-  }
+  const isLoading = userDataLoading || lettersLoading || isUserPending;
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
+  console.log('userid:', userid);
 
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <h1>마이페이지</h1>
+      {userData && (
+        <div>
+          <p>이메일: {userData.email}</p>
+          <p>닉네임: {userData.nickname}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MyPage;

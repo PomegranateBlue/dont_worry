3a9e9cc11e477b1d6009dc1ce4d9cd171c3c740d
mzsoba <owt4832@gmail.com>
2025-04-07 14:46:03 +0900

    ✨ feat/ 언더바가 아닌 배경 전체가 이동하게 변경

--- src/components/ranking/FilterMenu.tsx
+++ src/components/ranking/FilterMenu.tsx
@@ -18,11 +18,11 @@ const FilterMenu = () => {
         {CHART_FILTER_EMOTION}
       </Link>
       <motion.div
-        className="absolute bottom-0 h-1 bg-white"
-        layoutId="underline"
+        className="absolute inset-0 bg-black  -z-10"
+        layoutId="background"
         initial={false}
         animate={{
-          width: pathName === '/ranking' ? '40px' : '40px',
+          width: pathName === '/ranking' ? '42px' : '40px',
           x: pathName === '/ranking' ? '0%' : '138%'
         }}
         transition={{ type: 'spring', stiffness: 300, damping: 30 }}

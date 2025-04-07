6b19227754276606e4655290bc117de23219ada6
bluepome98 <wpdn1998@gmail.com>
2025-04-07 11:30:25 +0900

    � add 컴포넌트 동적 전환용 컴포넌트 생성

--- src/components/noteComponents/StepFlow.tsx
+++ src/components/noteComponents/StepFlow.tsx
@@ -1,11 +1,45 @@
 'use client';
 
 import { useState } from 'react';
-import CategoryForm from './CategoryForm';
+import EmotionCategoryForm from './EmotionCategoryForm';
+import TopicCategoryForm from './TopicCategoryForm';
 import MessageForm from './MessageForm';
-
+import ResultForm from './ResultForm';
 const StepFlow = () => {
-    const [step,setStep]
+  const [step, setStep] = useState<'emotion' | 'topic' | 'message' | 'result'>(
+    'emotion'
+  );
+
+  return (
+    <div>
+      {step === 'emotion' && (
+        <div>
+          <EmotionCategoryForm />
+          <button onClick={() => setStep('topic')}>다음으로</button>
+        </div>
+      )}
+
+      {step === 'topic' && (
+        <div>
+          <TopicCategoryForm />
+          <button onClick={() => setStep('message')}>다음으로</button>
+        </div>
+      )}
+
+      {step === 'message' && (
+        <div>
+          <MessageForm />
+          <button onClick={() => setStep('result')}>제출하기</button>
+        </div>
+      )}
+
+      {step === 'result' && (
+        <div>
+          <ResultForm />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default StepFlow;
--- src/components/noteComponents/TopicCategoryForm.tsx
+++ src/components/noteComponents/TopicCategoryForm.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import {
-  TOPIC_CATEGORIES,
-  EMOTION_CATEGORIES
-} from '@/constants/openai/category';
+import { TOPIC_CATEGORIES } from '@/constants/openai/category';
 import { useNoteStore } from '@/store/noteStore';
-const CategoryForm = () => {
-  const { selectedTopic, toggleTopic, selectedEmotions, toggleEmotion } =
-    useNoteStore();
+const TopicCategoryForm = () => {
+  const { selectedTopic, toggleTopic } = useNoteStore();
   return (
     <div>
       <p className="text-xl font-semibold p-4">무엇이 고민이신가요?</p>
@@ -27,30 +23,8 @@ const CategoryForm = () => {
           );
         })}
       </div>
-
-      <p className="text-xl font-semibold p-4">여러 감정이 들으셨을것 같네요</p>
-      <div className="p-4 flex flex-wrap gap-2">
-        {EMOTION_CATEGORIES.map((emotion) => {
-          const isSelected = selectedEmotions.includes(emotion);
-          return (
-            <button
-              key={emotion}
-              onClick={() => toggleEmotion(emotion)}
-              className={`px-4 py-2 border-[2px] font-bold rounded-3xl
-                ${
-                  isSelected
-                    ? 'bg-black text-white border-black'
-                    : 'bg-white text-black border-gray-400'
-                }
-              `}
-            >
-              {emotion}
-            </button>
-          );
-        })}
-      </div>
     </div>
   );
 };
 
-export default CategoryForm;
+export default TopicCategoryForm;

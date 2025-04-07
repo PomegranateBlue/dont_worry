1a68cecb10bdd23bb923500395dc2d85e3f04fb7
bluepome98 <wpdn1998@gmail.com>
2025-04-07 19:48:35 +0900

    🎉 add / 걱정 보관함 페이지 생성 후 기본 레이아웃 생성

---
+++ src/app/notebox/page.tsx
@@ -0,0 +1,9 @@
+const NoteBox = () => {
+  return (
+    <div className="mx-auto flex justify-center">
+      <p className="text-2xl font-bold p-4">걱정 보관함</p>
+    </div>
+  );
+};
+
+export default NoteBox;

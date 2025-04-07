ce8ae37b55c653e3f3971e9a99fb6ac8d3a7adf1
bluepome98 <wpdn1998@gmail.com>
2025-04-07 11:48:06 +0900

    ✨ update / GPT API 라우트 함수, 호출 함수 정의

---
+++ src/lib/api/gpt/gpt.ts
@@ -0,0 +1,24 @@
+export const fetchGPT = async ({
+  topic,
+  emotions,
+  message
+}: {
+  topic: string;
+  emotions: string[];
+  message: string;
+}) => {
+  const res = await fetch('/utils/openai', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      content: `주제: ${topic}, 감정: ${emotions.join(
+        ', '
+      )}, 메시지: ${message}`
+    })
+  });
+
+  const data = await res.json();
+  return data.content;
+};

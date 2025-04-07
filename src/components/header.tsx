75932647a98d5d0e53a1bbb2e88d10cf30e94712
hyerin-kang <hkang0420@gmail.com>
2025-04-07 17:00:12 +0900

    ✨ feat / 로그인,로그아웃 조건부렌더링

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,43 +1,28 @@
-'use client';
 import Link from 'next/link';
 import LogOutButton from './loginComponents/LogOutButton';
-import { useEffect, useState } from 'react';
-import { Session } from '@supabase/supabase-js';
-import { supabase } from '@/app/utils/supabase/supabase';
+import { getIsLogin } from '@/app/utils/supabase/server';
 
-export default function Header() {
-  const [session, setSession] = useState<Session | null>(null);
-  const isLogin = !!session;
-  useEffect(() => {
-    const {
-      data: { subscription }
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      if (_event == 'SIGNED_IN') {
-        setSession(session);
-        console.log('useEffect session', session);
-      }
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
+export default async function Header() {
+  const isLogin = await getIsLogin();
 
   return (
     <header className="flex flex-row flex-wrap bg-slate-200 p-4 justify-between">
       <Link href="/">서비스 소개</Link>
-      <Link href="/note">걱정 작성</Link>
-      <Link href="/">걱정 보관함</Link>
-      <Link href="/ranking">통계 페이지</Link>
-      <Link href="/letter">편지쓰기</Link>
-      <Link href="/mypage">마이페이지</Link>
+
       {isLogin ? (
-        <LogOutButton />
-      ) : (
         <>
+          <Link href="/note">걱정 작성</Link>
+          <Link href="/">걱정 보관함</Link>
+          <Link href="/ranking">통계 페이지</Link>
+          <Link href="/letter">편지쓰기</Link>
+          <Link href="/mypage">마이페이지</Link>
+          <LogOutButton />
+        </>
+      ) : (
+        <div className="space-x-4">
           <Link href="/auth/login">로그인</Link>
           <Link href="/auth/signup">회원가입</Link>
-        </>
+        </div>
       )}
     </header>
   );
--- src/components/loginComponents/LogOutButton.tsx
+++ src/components/loginComponents/LogOutButton.tsx
@@ -1,14 +1,15 @@
 'use client';
 import browserClient from '@/app/utils/supabase/client';
 import React from 'react';
-
+import { useRouter } from 'next/navigation';
 const LogOutButton = () => {
+  const router = useRouter();
   const signOut = async () => {
     const { error } = await browserClient.auth.signOut();
+    router.refresh();
     console.log(error);
     alert('로그아웃');
   };
   return <button onClick={signOut}>로그아웃</button>;
 };
-
 export default LogOutButton;

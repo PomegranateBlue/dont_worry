aa63444df8c126e861cb4288848eec728926f73e
mzsoba <owt4832@gmail.com>
2025-04-08 12:46:26 +0900

    ➖ delete/ 불필요한 부분 제거

--- src/components/ranking/WorsenedThing.tsx
+++ src/components/ranking/WorsenedThing.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import useAnalysisTrend from '@/app/utils/ranking/hooks/useAnalysisTrend';
 import { MOST_INCREASE_COMMENT } from '@/constants/ranking/Line';
+import useAnalysisTrend from '@/hooks/ranking/useAnalysisTrend';
 import { useRankingStore } from '@/store/ranking/rankingStore';
-
 
 import React from 'react';
 
@@ -11,7 +10,7 @@ const WorsenedThing = () => {
   const { year, month } = useRankingStore();
   const { data, loading, error } = useAnalysisTrend(year, month);
   if (loading) return <div className="p-4">데이터를 불러오는 중...</div>;
-  if (error) return <div className="p-4 text-red-500">{error}</div>;
+  if (error) console.log(error);
   if (!data) return <div className="p-4">표시할 데이터가 없습니다.</div>;
 
   const { mostIncreased } = data;

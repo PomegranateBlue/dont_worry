89bb1d64eb99cfad05194a67044e65fa73971585
bluepome98 <wpdn1998@gmail.com>
2025-04-08 12:45:54 +0900

    ✨ update / ResultForm 컴포넌트에서 로그인 한 사용자로의 데이터 전달 기능 구현

--- src/components/noteComponents/ResultForm.tsx
+++ src/components/noteComponents/ResultForm.tsx
@@ -1,23 +1,13 @@
 import { useNoteStore } from '@/store/noteStore';
-// import { useUserData } from '@/hooks/useMyPageQueries';
-import { useUserInfo } from '@/hooks/useMyPageQueries';
-
+import ResultSaveButton from './ResultSaveButton';
 const ResultForm = () => {
-  const { selectedTopic, selectedEmotions, message, result } = useNoteStore();
-
-  const handleSaveMessage = async (note: TablesInsert<'users_note'>) => {
-    const { data, error } = await supabase.from('users_note').insert(note);
-
-    if (error) throw error;
-
-    return data;
-  };
+  const { result } = useNoteStore();
 
   return (
     <div>
       <h2>걱숭이의 답변은?</h2>
       <p>{result || '...같이 고민하는 중'}</p>
-      <button onClick={handleSaveMessage}>저장하기</button>
+      <ResultSaveButton />
     </div>
   );
 };

91540466e2a1d87cd37a4b05b112287191b37d8c
mzsoba <owt4832@gmail.com>
2025-04-08 20:26:29 +0900

    ✨ feat/ 하드 코딩 텍스트 상수화

--- src/constants/ranking/Line.ts
+++ src/constants/ranking/Line.ts
@@ -1,6 +1,6 @@
 //하드 코딩 텍스트 상수화 파일
 
-export const CHART_PAGE_TITLE = '감정 레포트';
+export const CHART_PAGE_TITLE = '통합 레포트';
 
 export const CHART_FILTER_TOPIC = '주제별';
 
@@ -20,10 +20,22 @@ export const IS_LOADING = '데이터를 불러오는 중입니다...'; //임시 
 
 export const NO_DATA_CHART = `아직 걱정이 없으시군요! 걱정이 없으시다니 정말 다행이에요!`; //멘트 변경 필요
 
-export const MOST_DECREASE_COMMENT =
+export const MOST_DECREASE_COMMENT_MONTH =
   '에 관한 걱정이 전월 대비 가장 많이 감소했어요!';
 
-export const MOST_INCREASE_COMMENT =
+export const MOST_INCREASE_COMMENT_MONTH =
   '에 관한 걱정이 전월 대비 가장 많이 증가했어요!';
 
-export const REPORT_TITLE = '하이라이트';
+export const MOST_DECREASE_COMMENT_WEEK =
+  '에 관한 걱정이 지난주 대비 가장 많이 감소했어요!';
+
+export const MOST_INCREASE_COMMENT_WEEK =
+  '에 관한 걱정이 지난주 대비 가장 많이 증가했어요!';
+
+export const REPORT_TITLE_MONTH = '이 달의 하이라이트';
+
+export const REPORT_TITLE_WEEK = '이 주의 하이라이트';
+
+export const MONTH = '월';
+
+export const WEEK = '주';

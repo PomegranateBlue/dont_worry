8ea33f84f06aa9223ca9be3b77a309f84ac2c2c6
ick-web <yih000098@gmail.com>
2025-04-09 16:10:09 +0900

    ✨ update: 프로필사진업로드,수정,삭제 기능 구현

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,26 +1,54 @@
 'use client';
 
 import NicknameEditModal from '@/components/mypage/NicknameEditModal';
+
+import ProfileImage from '@/components/mypage/ProfileImage';
 import {
+  useUpdateUserInfo,
   useUserData,
   useUserInfo,
   useUserLetters,
   useUserWorries
 } from '@/hooks/useMyPageQueries';
 import { useUserStore } from '@/store/store';
-import { PencilLine } from 'lucide-react';
-import Image from 'next/image';
+import { ChevronRight, PencilLine } from 'lucide-react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { uploadProfileImage } from '../utils/supabase/db';
+import { useQueryClient } from '@tanstack/react-query';
+import LogOutButton from '@/components/loginComponents/LogOutButton';
 
 const MyPage = () => {
+  const queryClient = useQueryClient();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { setUser } = useUserStore();
+  const { user, setUser } = useUserStore();
+  const { mutateAsync: updateUserInfo } = useUpdateUserInfo();
   const { data: letters, isLoading: lettersLoading } = useUserLetters();
   const { data: userInfo, isLoading: userDataLoading } = useUserInfo();
   const { data: userWorries, isLoading: userWorriesLoading } = useUserWorries();
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
+
+  const handleUpload = async (file: File) => {
+    try {
+      if (!user) return;
+      const url = await uploadProfileImage(file, user);
+      await updateUserInfo({ profile_img: url });
+    } catch (err) {
+      alert('이미지 업로드 실패 ');
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await updateUserInfo({ profile_img: null });
+      queryClient.invalidateQueries({ queryKey: ['userinfo', user] });
+    } catch (err) {
+      alert('이미지 삭제 실패 ');
+      console.log(err);
+    }
+  };
 
   const {
     data: userid,
@@ -37,7 +65,7 @@ const MyPage = () => {
 
   const isLoading =
     userDataLoading || lettersLoading || isUserPending || userWorriesLoading;
-  console.log('page.tsx userData$$', userInfo);
+  console.log('page.tsx userInfo$$', userInfo);
 
   if (isUserError) {
     return <div>사용자 정보를 가져오는 중 오류가 발생했습니다.</div>;
@@ -51,24 +79,11 @@ const MyPage = () => {
     <div className="px-4 pb-20">
       {/* 프로필 섹션 */}
       <div className="flex flex-col items-center py-6">
-        {userInfo?.profile_img && (
-          <div className="relative w-28 h-28 rounded-full overflow-hidden">
-            <Image
-              src={userInfo.profile_img}
-              alt="프로필 이미지"
-              fill
-              className="object-cover"
-              sizes="112px"
-            />
-          </div>
-        )}
-        {/* 프로필 이미지 */}
-        <div className="relative w-28 h-28 rounded-full bg-gray-200 border overflow-hidden">
-          {/* 수정 아이콘 */}
-          <div className="absolute bottom-0 right-0 bg-black w-8 h-8 rounded-full flex items-center justify-center">
-            <PencilLine color="gray" size={16} />
-          </div>
-        </div>
+        <ProfileImage
+          imageUrl={userInfo?.profile_img || undefined}
+          onUpload={handleUpload}
+          onDelete={handleDelete}
+        />
 
         {/* 닉네임 */}
         <div className="flex items-center gap-1 mt-4">
@@ -86,11 +101,13 @@ const MyPage = () => {
       {/* 작성글/편지 정보 */}
       <div className="flex justify-center items-center rounded-2xl bg-gray-50 py-4 mb-6">
         <div className="flex-1 text-center">
-          <p className="text-lg font-semibold">
-            {/* 걱정 갯수 넣기 */}
-            {userWorries?.length || 0}개
-          </p>
-          <p className="text-xs text-gray-500">작성한 걱정</p>
+          <Link href="/notebox">
+            <p className="text-lg font-semibold">
+              {/* 걱정 갯수 넣기 */}
+              {userWorries?.length || 0}개
+            </p>
+            <p className="text-xs text-gray-500">작성한 걱정</p>
+          </Link>
         </div>
         <div className="w-px h-10 bg-gray-200" />
         <div className="flex-1 text-center">
@@ -99,6 +116,27 @@ const MyPage = () => {
             <p className="text-xs text-gray-500">미래 편지</p>
           </Link>
         </div>
+      </div>
+      <div className="border-t border-b pb-16 pt-4 text-sm text-gray-700 space-y-4">
+        {[
+          ['공지사항', '/mypage/notice'],
+          ['이용약관', '/mypage/terms'],
+          ['챗봇 문의', '/mypage/chat'],
+          ['버전', '/mypage/version']
+        ].map(([label, link], idx) => (
+          <Link
+            key={idx}
+            href={link}
+            className="flex justify-between items-center"
+          >
+            <span>{label}</span>
+            <ChevronRight size={16} color="gray" />
+          </Link>
+        ))}
+      </div>
+      <div className='flex flex-row justify-center gap-12 mt-4 text-gray-500'>
+        <LogOutButton/>
+        <p>회원탈퇴</p>
       </div>
     </div>
   );

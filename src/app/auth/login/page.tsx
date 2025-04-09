04a9bf074143e1cf157eea60d957b24e9485f198
hyerin-kang <hkang0420@gmail.com>
2025-04-09 15:34:23 +0900

    💄style / 소셜로그인 아이콘 추가

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,6 +5,7 @@ import { login } from '../action';
 import { useForm } from 'react-hook-form';
 import { useFormState } from 'react-dom';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const initialState = { error: null };
 
@@ -32,8 +33,11 @@ export default function LoginPage() {
 
   return (
     <>
-      <h2 className="m-4 text-center font-semibold">로그인</h2>
-      <form action={formAction} className="space-y-2">
+      <h2 className="m-4 text-center font-semibold text-xl">로그인</h2>
+      <form
+        action={formAction}
+        className="space-y-2 border-b border-b[#E0E0E2] pb-6"
+      >
         <div>
           <label>
             <span className="font-semibold">이메일</span>
@@ -44,7 +48,9 @@ export default function LoginPage() {
               id="email"
               placeholder="ex)abc@email.com"
               required
-              className="w-full p-4 border-b-[1px] border-b-[#D6D6D6]"
+              className={`w-full p-4 border-b-[1px] focus:outline-none ${
+                errors.email ? 'border-b-red-500' : 'border-b-[#D6D6D6]'
+              }`}
             />
             {errors.email && (
               <p className="text-red-500 mt-1">{errors.email.message}</p>
@@ -53,14 +59,16 @@ export default function LoginPage() {
         </div>
         <div>
           <label>
-            비밀번호
+            <span className="font-semibold">비밀번호</span>
             <input
               {...register('password')}
               type="password"
               name="password"
               placeholder="비밀번호 입력"
               required
-              className="w-full p-4 border-b-[1px] border-b-[#D6D6D6]"
+              className={`w-full p-4 border-b-[1px] focus:outline-none ${
+                errors.password ? 'border-b-red-500' : 'border-b-[#D6D6D6]'
+              }`}
             />
             {errors.password ? (
               <p className="text-red-500 mt-1">{errors.password.message}</p>
@@ -76,20 +84,41 @@ export default function LoginPage() {
 
         <button
           type="submit"
-          className="w-full bg-black p-3 rounded-sm !mt-10 text-white"
+          className="w-full bg-[#8573C9] p-3 rounded-md !mt-10 text-white text-lg font-medium"
         >
           로그인
         </button>
       </form>
       <div className="mt-10">
         <p className="text-lg text-center">소셜 계정으로 로그인</p>
-        <ul className="flex flex-wrap mx-auto w-fit space-x-9 m-8">
-          <li className="w-[52px] h-[52px] bg-gray-300 rounded-full"></li>
-          <li className="w-[52px] h-[52px] bg-gray-300 rounded-full"></li>
-          <li className="w-[52px] h-[52px] bg-gray-300 rounded-full"></li>
-        </ul>
+        <div className="flex flex-wrap mx-auto w-fit space-x-9 m-8">
+          <Image
+            src="/images/login-kakao.svg"
+            width={56}
+            height={56}
+            alt="login-kakao"
+            priority
+          />
+          <Image
+            src="/images/login-naver.svg"
+            width={56}
+            height={56}
+            alt="login-naver"
+            priority
+          />
+          <Image
+            src="/images/login-google.svg"
+            width={56}
+            height={56}
+            alt="login-google"
+            priority
+          />
+        </div>
       </div>
-      <Link href="/auth/signup" className="text-center mx-auto w-fit block">
+      <Link
+        href="/auth/signup"
+        className="text-center mx-auto w-fit block underline"
+      >
         회원가입
       </Link>
     </>

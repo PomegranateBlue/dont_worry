036c638f898c1811c9b8399782ab1d76708d1129
bluepome98 <wpdn1998@gmail.com>
2025-04-10 17:38:05 +0900

    🩹 fix / textarea 글자 수 조정 값 오류 수정

--- src/components/noteComponents/MessageForm.tsx
+++ src/components/noteComponents/MessageForm.tsx
@@ -11,7 +11,7 @@ const MessageForm = () => {
         className="p-2 border-[1px] border-black text-black resize-none w-full h-96 rounded-lg"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
-        max-length={150}
+        maxLength={150}
         placeholder="최대 150자 입력 가능합니다"
       ></textarea>
     </div>

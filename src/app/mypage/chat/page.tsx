0a3f8e7c0a54a1dce25071b91df3b24deb37151a
ick-web <yih000098@gmail.com>
2025-04-10 20:20:06 +0900

    💄 style: css 수정 뒤로가기 아이콘으로 변경

--- src/app/mypage/chat/page.tsx
+++ src/app/mypage/chat/page.tsx
@@ -1,13 +1,29 @@
+import { ChevronLeft } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const ChatPage = () => {
   return (
-    <div>
-      <h1 className="text-xl m-4 text-center">서비스 준비 중입니다.</h1>
-      <Link href="/mypage" className="border rounded-md p-2 mt-4 mb-4">
-        뒤로가기
-      </Link>
+    <div className="flex flex-col min-h-screen">
+      {/* 왼쪽 상단 뒤로가기 아이콘 */}
+      <div className="relative flex items-center justify-center h-16 border-b">
+        <Link href="/mypage" className="absolute left-4 p-2">
+          <ChevronLeft size={24} />
+        </Link>
+        <h1 className="text-xl font-semibold">서비스 준비 중입니다.</h1>
+      </div>
+
+      {/* 가운데 콘텐츠 */}
+      <div className="flex flex-col items-center flex-1 mt-4">
+        <Image
+          width={150}
+          height={100}
+          src="/images/Sad.svg"
+          alt="슬픔 이미지"
+          priority
+        />
+      </div>
     </div>
   );
 };

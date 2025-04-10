d32876f12b32ab441a24e278a909146a3ea79318
mzsoba <owt4832@gmail.com>
2025-04-10 11:32:11 +0900

    ✨ feat/ 주석 추가

--- src/components/ranking/FusionComp/MWNavigator.tsx
+++ src/components/ranking/FusionComp/MWNavigator.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import MonthNavigator from '../MonthNavigator';
 import WeekNavigator from '../WeekNavigator';
 import { useRankingStore } from '@/store/ranking/rankingStore';
-
+//todo: timefilter.tsx완성 후 삭제하기
 const MWNavigator = () => {
   const { mode } = useRankingStore();
   const { year, month, week } = useRankingStore();

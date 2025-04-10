f630611fb402834e2f52efc9b59ee8b21b39a22f
mzsoba <owt4832@gmail.com>
2025-04-10 20:59:01 +0900

    ✨ faet/ persist로 감싸기 밑 user기본값을 로컬스토리지에서 가져오게 수정

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,25 @@
 import { create } from 'zustand';
+// import { devtools } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
 type UserState = {
   user: string | null;
   setUser: (user: string | null) => void;
 };
 
-export const useUserStore = create<UserState>((set) => ({
-  user: null,
-  setUser: (user) => set({ user })
-}));
+// export const useUserStore = create<UserState>((set) => ({
+//   user: null,
+//   setUser: (user) => set({ user })
+// }));
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null, //localStorage.getItem('auth-storage'), //이 부분이 의심
+      setUser: (user) => set({ user })
+    }),
+    {
+      name: 'auth-storage'
+    }
+  )
+);

cac963c2a3bec919ac751765c92904dd45e2c63c
bluepome98 <wpdn1998@gmail.com>
2025-04-11 11:28:23 +0900

    🩹 fix / db.ts fetchUserWorries id=> user_id 로 변경

--- src/components/noteComponents/ResultSaveButton.tsx
+++ src/components/noteComponents/ResultSaveButton.tsx
@@ -2,24 +2,25 @@
 
 import { useNoteStore } from '@/store/noteStore';
 import { supabase } from '@/app/utils/supabase/supabase';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TablesInsert } from '../../../database.types';
 import { useUserStore } from '@/store/store';
 import { ThumbsUp } from 'lucide-react';
 // import { useUserData } from '@/hooks/useMyPageQueries';
+// import browserClient from '@/app/utils/supabase/client';
 
 const ResultSaveButton = () => {
   const { selectedTopic, selectedEmotions, message, result } = useNoteStore();
   const { user } = useUserStore();
   const [isSaved, setIsSaved] = useState(false);
-  // const loginUser = useUserData();
-
-  // console.log(loginUser);
+  // const [userId, setUserId] = useState<string | null>(null);
 
   const handleSaveMessage = async () => {
     console.log('저장되었습니다');
-    console.log(message);
-    console.log(result);
+    console.log(user);
+
+    // console.log(message);
+    // console.log(result);
     // console.log(user);
 
     if (!user) {
@@ -31,14 +32,9 @@ const ResultSaveButton = () => {
       emotion_category: selectedEmotions.join(','),
       created_at: new Date().toISOString(),
       note_img: null,
-      id: user
+      user_id: user
     };
 
-    // console.log('111111111111111', selectedTopic);
-    // console.log('222222222222222222', selectedEmotions);
-    // console.log('33333333333333333333', result);
-
-    console.log('#############', note);
     const { error } = await supabase.from('users_note').insert([note]);
 
     if (error) {

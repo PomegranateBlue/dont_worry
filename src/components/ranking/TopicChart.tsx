cdd8cafe72a206e5ae6007a509ce4476916130b0
mzsoba <owt4832@gmail.com>
2025-04-15 12:46:35 +0900

    ✨ feat/ 차트에 플러그인 추가, 텍스트가 항목별로 보임

--- src/components/ranking/TopicChart.tsx
+++ src/components/ranking/TopicChart.tsx
@@ -10,13 +10,19 @@ import {
   ChartOptions,
   ChartData
 } from 'chart.js';
-import { backgroundColor, borderColor } from '@/constants/ranking/ChartOptions';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
 import {
   TOPIC_FILTER_DESCRIPTION,
   TOPIC_FILTER_DESCRIPTION2
 } from '@/constants/ranking/Line';
+import {
+  CHART_LABEL,
+  topicBackgroundColor,
+  topicBorderColor
+} from '@/constants/ranking/ChartOptions';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 interface ChartProps {
   topTopics: { name: string; count: number }[];
@@ -25,16 +31,16 @@ interface ChartProps {
 const TopicChart: React.FC<ChartProps> = ({ topTopics }) => {
   const topicLabels = topTopics.map((topic) => topic.name);
   const topicData = topTopics.map((topic) => topic.count);
+  const total = topicData.reduce((acc, cur) => acc + cur, 0); // 총합
 
-  // 주제별 차트 데이터
   const topicChartData: ChartData<'doughnut'> = {
     labels: topicLabels,
     datasets: [
       {
-        label: 'Top Topics',
+        label: CHART_LABEL,
         data: topicData,
-        backgroundColor: backgroundColor,
-        borderColor: borderColor,
+        backgroundColor: topicBackgroundColor,
+        borderColor: topicBorderColor,
         borderWidth: 2
       }
     ]
@@ -46,21 +52,36 @@ const TopicChart: React.FC<ChartProps> = ({ topTopics }) => {
       legend: {
         position: 'bottom',
         display: false
+      },
+      datalabels: {
+        color: '#171719',
+        font: {
+          weight: 'bold',
+          size: 12
+        },
+        formatter: (value, context) => {
+          const label = context.chart.data.labels?.[context.dataIndex];
+          const percent = ((value / total) * 100).toFixed(0);
+          return `${label}\n (${percent}%)`;
+        },
+        align: 'center',
+        textAlign: 'center',
+        clip: false
       }
     }
   };
 
   return (
-    <div className="w-full max-w-md mx-auto my-6 p-6 bg-white rounded-2xl flex flex-col items-center">
+    <div className="w-full max-w-md mx-auto my-6 bg-white rounded-2xl flex flex-col items-center">
       <div className="mb-6 text-center">
-        <h2 className="text-xl font-bold text-gray-900">
+        <h2 className="text-xl font-bold text-label-normal">
           {TOPIC_FILTER_DESCRIPTION}
         </h2>
         <p className="text-sm text-gray-500 mt-1">
           {TOPIC_FILTER_DESCRIPTION2}
         </p>
       </div>
-      <div className="w-[280px] h-[280px] sm:w-[320px] sm:h-[320px]">
+      <div className="w-[217.8px] h-[198px] sm:w-[320px] sm:h-[320px]">
         <Doughnut options={options} data={topicChartData} />
       </div>
     </div>

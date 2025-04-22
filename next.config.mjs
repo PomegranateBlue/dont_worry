29f30847cb35e66a90a242b999df252c190c4dcb
hyerin-kang <hkang0420@gmail.com>
2025-04-22 17:46:48 +0900

    🔨  add / 구글로그인 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['pzyuqsztorznxejhknhd.supabase.co', 'k.kakaocdn.net']
+    domains: [
+      'pzyuqsztorznxejhknhd.supabase.co',
+      'k.kakaocdn.net',
+      'lh3.googleusercontent.com'
+    ]
   }
 };
 

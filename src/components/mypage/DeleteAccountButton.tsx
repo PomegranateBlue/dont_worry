45eba3bbcbc3ec8cec00231ca77ad8f5726ca9aa
ick-web <yih000098@gmail.com>
2025-04-24 16:26:55 +0900

    ♻️ refactor: 직접 불러오지않고 업데이트로직으로 변경

--- src/components/mypage/DeleteAccountButton.tsx
+++ src/components/mypage/DeleteAccountButton.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation';
 import { useUserStore } from '@/store/auth/store';
 import { useState } from 'react';
+import { updateUserInfo } from '@/app/utils/supabase/db';
 import browserClient from '@/app/utils/supabase/client';
 import Text from '../common/Text';
 
@@ -19,22 +20,22 @@ export default function DeleteAccountButton() {
 
     setLoading(true);
 
-    const { error } = await browserClient
-      .from('users')
-      .update({ is_deleted: true }) // 소프트 삭제
-      .eq('user_id', user);
+    try {
+      // 사용자 소프트 삭제
+      const updatedUser = await updateUserInfo(user, { is_deleted: true });
+      if (!updatedUser) {
+        throw new Error('사용자 정보 업데이트 실패');
+      }
 
-    if (error) {
+      await browserClient.auth.signOut();
+      setUser(null);
+      router.push('/');
+    } catch (error) {
       alert('회원탈퇴 중 오류가 발생했습니다.');
       console.error('삭제 오류:', error);
+    } finally {
       setLoading(false);
-      return;
     }
-
-    // 로그아웃 처리
-    await browserClient.auth.signOut();
-    setUser(null); // Zustand 상태 초기화
-    router.push('/'); // 홈으로 이동
   };
 
   return (

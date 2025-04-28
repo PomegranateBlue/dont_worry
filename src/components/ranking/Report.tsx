3ae6eb9ce495c726d419588ba58e24d80aeb22da
mzsoba <owt4832@gmail.com>
2025-04-28 20:51:14 +0900

    ✨ feat/ 메모이제이션으로 성능 개선

--- src/components/ranking/Report.tsx
+++ src/components/ranking/Report.tsx
@@ -1,3 +1,4 @@
+// components/ranking/Report.tsx
 import { Most } from '@/types/ranking/types';
 import React from 'react';
 import Text from '../common/Text';
@@ -7,22 +8,24 @@ interface ReportProps {
 }
 
 const Report = ({ most }: ReportProps) => {
+  if (!most) {
+    return (
+      <div className="h-[100px] w-full bg-gray-100 animate-pulse rounded-md" />
+    );
+  }
+
   return (
-    <>
-      {most && (
-        <Text
-          as="h2"
-          variant="heading3"
-          variant2="heading4"
-          color="label-normal"
-          className="text-center break-words"
-        >
-          {`${most.name}에 관한 걱정이`}
-          <br />
-          {`${most.percentage}%로 가장 많았어요`}
-        </Text>
-      )}
-    </>
+    <Text
+      as="h2"
+      variant="heading3"
+      variant2="heading4"
+      color="label-normal"
+      className="text-center break-words"
+    >
+      {`${most.name}에 관한 걱정이`}
+      <br />
+      {`${most.percentage}%로 가장 많았어요`}
+    </Text>
   );
 };
 
